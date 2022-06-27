b1c8404e9c83356a8164a05312124882b7d5b51e
Sergio Henrique <sergio.henrique@fde.sp.gov.br>
2022-06-27 17:11:09 -0300

    reiniciando projeto

---
+++ docker_config/postgres/init.sql
@@ -0,0 +1,6 @@
+SELECT 'CREATE USER klanadmin' where not exists (select from pg_user where usename = 'klanadmin')\gexec
+
+SELECT 'CREATE DATABASE klan'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'klan')\gexec
+
+GRANT ALL PRIVILEGES ON DATABASE klan TO klanadmin;
